c6006f03561ec7da8ed7109122e3d15e0dec496f
KABANGA David <50810707+tipizzo@users.noreply.github.com>
2026-05-22 16:32:13 +0200

    Adding comments for unused div in index

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
     container: 'map',
     style: 'https://api.maptiler.com/maps/streets-v4/style.json?key=QjCZXnsGZAfLPiMGCTBy',
-    center: [29.2228, -1.6792], // Zooming sur Goma
+    center: [29.2228, -1.6792], // Localisation sur Goma
     zoom: 12,
     attributionControl: false
 })
