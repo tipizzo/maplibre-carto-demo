f0723705411cb860033128e485536dc58c731169
KABANGA David <50810707+tipizzo@users.noreply.github.com>
2026-05-23 04:25:51 +0200

    Displaying data from a geojson file and adding search by gref functionnality

--- app.js
+++ app.js
@@ -10,3 +10,97 @@ map.addControl(new maplibregl.AttributionControl({
     compact: true
 }), 'bottom-left');
 
+map.on('load', () => {
+    map.addSource('goma-buildings', {
+        type: 'geojson',
+        data: './building_eligibility_goma.geojson'
+    });
+
+    map.addLayer({
+        id: 'buildings-layer',
+        type: 'fill',
+        source: 'goma-buildings',
+        paint: {
+            'fill-color': [
+                'match',
+                ['get', 'is_eligible'],
+                'true', '#198ceb',
+                'false', '#e74c3c',
+                '#95a5a6'
+            ],
+            'fill-opacity': 0.75,
+            'fill-outline-color': '#1a1a1a'
+        }
+    })
+
+    map.addLayer({
+        id: 'buildings-labels-layer',
+        type: 'symbol',
+        source: 'goma-buildings',
+        minzoom: 16,
+        layout: {
+            'text-field': ['get', 'gref'],
+            'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+            'text-size': 11,
+            'text-anchor': 'center',
+            'text-justify': 'center',
+            'text-allow-overlap': false
+        },
+
+        paint: {
+            'text-color': '#000000',
+            'text-halo-color': '#ffffff',
+            'text-halo-width': 1.5
+        }
+    })
+
+    initSearchFeature();
+})
+
+function initSearchFeature() {
+    const searchInput = document.getElementById('search-input-2')
+    const searchButton = document.querySelector('.search-button');
+
+    searchButton.addEventListener('click', () => {
+        performSearch(searchInput.value)
+    })
+
+    searchInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            performSearch(searchInput.value)
+        }
+    })
+}
+
+function performSearch(query) {
+    const value = query.trim();
+    if (!value) return;
+
+    const features = map.querySourceFeatures('goma-buildings');
+
+    const targetBuilding = features.find(f => f.properties.gref && String(f.properties.gref) === value);
+
+    if (targetBuilding) {
+        let coordinates;
+
+        if (targetBuilding.geometry.type === 'Polygon') {
+            coordinates = targetBuilding.geometry.coordinates[0][0];
+        } else if (targetBuilding.geometry.type === 'MultiPolygon') {
+            coordinates = targetBuilding.geometry.coordinates[0][0][0];
+        } else if (targetBuilding.geometry.type === 'Point') {
+            coordinates = targetBuilding.geometry.coordinates
+        }
+
+        if (coordinates) {
+            map.flyTo({
+                center: coordinates,
+                zoom: 20,
+                essential: true
+            })
+        }
+
+        else {
+            alert('Batiment introuvable pour: "${value}')
+        }
+    }
+}
